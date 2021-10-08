2dd075b14f981bce0c5cbdac5be0095c406697c0
Balázs Veréb <ipponbalazs@gmail.com>
2021-10-08 22:55:57 +0200

    Excel munkafüzet létrehozása és megnyitása

--- week04helyes/week04helyes/Form1.cs
+++ week04helyes/week04helyes/Form1.cs
@@ -16,6 +16,9 @@ namespace week04helyes
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xLSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +28,31 @@ namespace week04helyes
         {
             lakasok = context.Flats.ToList();
 
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application(); // Excel elindítása és az applikáció objektum betöltése
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xLSheet = xlWB.ActiveSheet;
+
+                //CreateTabel();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errorMSG = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            }
         }
     }
 }
