376a6393260700e8e0a21854fb0b9d59cc0e705f
Balázs Veréb <ipponbalazs@gmail.com>
2021-10-08 10:19:41 +0200

    4.

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -12,9 +12,20 @@ namespace week04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities1 context = new RealEstateEntities1();
+        List<Flat> lakasok;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+
+
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+        
         }
     }
 }
