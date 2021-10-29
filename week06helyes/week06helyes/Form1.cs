cd7bbebd15e8527c314f89d20fd3d31e1566456d
Balázs Veréb <ipponbalazs@gmail.com>
2021-10-29 16:05:20 +0200

    untiltask5

--- week06helyes/week06helyes/Form1.cs
+++ week06helyes/week06helyes/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
 using week06helyes.Entities;
 using week06helyes.MnbServiceReference;
 
@@ -15,40 +10,28 @@ namespace week06helyes
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates = new BindingList<RateData>();
-        
-        
+        BindingList<RateData> Rates;
         public Form1()
         {
             InitializeComponent();
-
-           
-            EuroCurrency2020();
-            //var mnbService = new MNBArfolyamServiceSoapClient();
-            //var request = new GetCurrenciesRequestBody();
-            //var response = mnbService.GetCurrencies(request);
-            //var result = response.GetCurrenciesResult;
             dataGridView1.DataSource = Rates;
-            
+            CallService();
+
         }
 
-
-
-        private string EuroCurrency2020()
+        void CallService()
         {
-           
-                var mnbService = new MNBArfolyamServiceSoapClient();
-                var request = new GetExchangeRatesRequestBody
-                {
-                    currencyNames = "EUR",
-                    startDate = "2020-01-01",
-                    endDate = "2020-06-30"
-                };
-                var response = mnbService.GetExchangeRates(request);
-                var result = response.GetExchangeRatesResult;
-                return result;
-            
 
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            Console.WriteLine(result);
         }
     }
 }
