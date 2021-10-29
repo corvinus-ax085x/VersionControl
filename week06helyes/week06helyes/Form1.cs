8ffe5cc45d4317a5929d11650ae401897eec3f83
Balázs Veréb <ipponbalazs@gmail.com>
2021-10-29 15:59:04 +0200

    task4

--- week06helyes/week06helyes/Form1.cs
+++ week06helyes/week06helyes/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using week06helyes.Entities;
+using week06helyes.MnbServiceReference;
 
 namespace week06helyes
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
+        
         public Form1()
         {
             InitializeComponent();
+
+           
+            EuroCurrency2020();
+            //var mnbService = new MNBArfolyamServiceSoapClient();
+            //var request = new GetCurrenciesRequestBody();
+            //var response = mnbService.GetCurrencies(request);
+            //var result = response.GetCurrenciesResult;
+            dataGridView1.DataSource = Rates;
+            
+        }
+
+
+
+        private string EuroCurrency2020()
+        {
+           
+                var mnbService = new MNBArfolyamServiceSoapClient();
+                var request = new GetExchangeRatesRequestBody
+                {
+                    currencyNames = "EUR",
+                    startDate = "2020-01-01",
+                    endDate = "2020-06-30"
+                };
+                var response = mnbService.GetExchangeRates(request);
+                var result = response.GetExchangeRatesResult;
+                return result;
+            
+
         }
     }
 }
