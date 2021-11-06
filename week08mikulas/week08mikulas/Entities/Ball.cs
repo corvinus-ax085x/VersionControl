82f28e60c1ed951a3f3add7ac2a4aa54372db1e3
Balázs Veréb <ipponbalazs@gmail.com>
2021-11-06 12:43:08 +0100

    Absztrakció

--- week08mikulas/week08mikulas/Entities/Ball.cs
+++ week08mikulas/week08mikulas/Entities/Ball.cs
@@ -8,30 +8,13 @@ using System.Windows.Forms;
 
 namespace week08mikulas.Entities
 {
-    public class Ball:Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = Height;
-            Paint += Ball_Paint;
-        
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+        
     }
 }
