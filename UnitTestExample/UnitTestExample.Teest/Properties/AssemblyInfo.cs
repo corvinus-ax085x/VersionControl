547b55ed07c873cac5ba75f93a839b8140e36e6c
Balázs Veréb <ipponbalazs@gmail.com>
2021-11-24 16:26:40 +0100

    NuGet csomagok telepítése, Email validálás teszt függvény

--- UnitTestExample/UnitTestExample.Teest/Properties/AssemblyInfo.cs
+++ UnitTestExample/UnitTestExample.Teest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyTitle("UnitTestExample.Teest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyProduct("UnitTestExample.Teest")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("a79c00ac-bb4d-49fc-828c-7a52a6c3a2d1")]
+[assembly: Guid("2da64bea-b645-42b5-b50d-c9f7128a563e")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
