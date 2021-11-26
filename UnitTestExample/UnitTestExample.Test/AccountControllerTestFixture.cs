a864c7384691603300011c522a853ffc3ac2bb57
Balázs Veréb <ipponbalazs@gmail.com>
2021-11-26 12:40:06 +0100

    Alosztály hívás kezelése

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,11 +1,14 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test
 {
@@ -62,7 +65,12 @@ namespace UnitTestExample.Test
         public void TestRegisterHappyPath(string email, string password)
         {
             //Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Returns<Account>(a => a);
             var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
 
 
             //Act
@@ -73,21 +81,23 @@ namespace UnitTestExample.Test
             Assert.AreEqual(email, actualResult.Email);
             Assert.AreEqual(password, actualResult.Password);
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+            accountServiceMock.Verify(m => m.CreateAccount(actualResult), Times.Once);
         }
 
         [
             Test,
-            TestCase("irf@uni-corvinus", "Abcd1234"),
-            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
-            TestCase("irf@uni-corvinus.hu", "abcd1234"),
-            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
-            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
-            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234")
         ]
         public void TestRegisterValidateException(string email, string password)
         {
             // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
             var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
 
             // Act
             try
