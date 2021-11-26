ada67e3bf138a821fe22bc5fbb3a581d0c0d6727
Balázs Veréb <ipponbalazs@gmail.com>
2021-11-26 12:02:32 +0100

    Jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -30,5 +30,27 @@ namespace UnitTestExample.Test
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+
+        [
+           Test,
+           TestCase("abcd1234", "false"),
+           TestCase("ABCS1234", "false"),
+           TestCase("ABcd1234", "true"),
+           TestCase("AB1234", "true"),
+       ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
