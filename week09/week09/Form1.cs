2f306a11195ce815e4fdf45c2cbac0fa61911be9
Balázs Veréb <ipponbalazs@gmail.com>
2021-12-03 10:43:09 +0100

    Véletlenszám generálás

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -14,6 +14,8 @@ namespace week09
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = null;
         List<BirthProbability> BirthProbabilities = null;
         List<DeathProbability> DeathProbabilities = null;
