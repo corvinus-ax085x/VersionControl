29af79f68120568b05e30ac0a2281ef239a1bee5
Balázs Veréb <ipponbalazs@gmail.com>
2021-12-10 10:11:10 +0100

    Játéktér és játék kiptóbálása

---
+++ EvolAlgorithm/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorldsHardestGame;
+
+namespace EvolAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        GameController gc = new GameController();
+        GameArea ga = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
+        }
+    }
+}
