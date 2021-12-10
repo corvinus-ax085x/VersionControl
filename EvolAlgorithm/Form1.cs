d67ad352b782bf81442851d68ace1127397e1194
Balázs Veréb <ipponbalazs@gmail.com>
2021-12-10 10:14:44 +0100

    Induló populáció felépítése

--- EvolAlgorithm/Form1.cs
+++ EvolAlgorithm/Form1.cs
@@ -15,6 +15,11 @@ namespace EvolAlgorithm
     {
         GameController gc = new GameController();
         GameArea ga = null;
+
+        int populationSize = 100;
+        int nbrOfDtepd = 10;
+        int nbrOfStepsncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
